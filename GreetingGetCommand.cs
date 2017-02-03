f1b540c93267344426f90d8caa4695df55e3bf74
Joel Bennett <Jaykul@HuddledMasses.org>
2017-02-03 01:22:56 -0500

    initial commit

---
+++ GreetingGetCommand.cs
@@ -0,0 +1,16 @@
+﻿using System.Management.Automation;
+
+namespace NetCoreModuleProof
+{
+    [Cmdlet(VerbsCommon.Get, "Greeting")]
+    public class GreetingGetCommand : Cmdlet
+    {
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+
+            WriteObject("Hello World");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyCompany("HuddledMasses.org")]
+[assembly: AssemblyProduct("NetCoreModuleProof")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a35b19c0-de42-438e-9a79-2ac07cf2fc53")]
